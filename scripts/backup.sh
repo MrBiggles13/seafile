5e326f5642d1ae904bc1d71a92566815de7e4824
MrBiggles13 <131645558+MrBiggles13@users.noreply.github.com>
2026-02-06 15:02:33 +0100

    Update backup.sh

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -9,7 +9,7 @@ mkdir -p "$OUT"
 
 echo "Dumping DB..."
 DB_ROOT_PASSWORD="$(grep -E '^DB_ROOT_PASSWORD=' .env | cut -d= -f2-)"
-docker compose exec -T db sh -lc 'mariadb-dump -uroot -p"$MYSQL_ROOT_PASSWORD" --single-transaction seafile'   > "$OUT/seafile.sql"
+docker compose exec -T db sh -lc 'mariadb-dump -uroot -p"$MYSQL_ROOT_PASSWORD" --single-transaction seafile_db'   > "$OUT/seafile.sql"
 
 echo "Saving config..."
 tar -czf "$OUT/config.tgz" config monitoring .env docker-compose.yml
